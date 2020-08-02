27b0695d984eedd5d12a93ab3c45127dcb566e08
Jozef Izso <jozef.izso@gmail.com>
2020-08-02 20:44:21 +0200

    Generate sample Excel file

--- ImageToExcel/Program.cs
+++ ImageToExcel/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace ImageToExcel
 {
@@ -6,7 +9,51 @@ namespace ImageToExcel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var filename = "Image.xlsx";
+
+            var spreadsheetDocument = SpreadsheetDocument.Create(filename, SpreadsheetDocumentType.Workbook);
+            var workbookpart = spreadsheetDocument.AddWorkbookPart();
+            workbookpart.Workbook = new Workbook();
+
+            var worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+            worksheetPart.Worksheet = new Worksheet(new SheetData());
+            var sheets = spreadsheetDocument.WorkbookPart.Workbook.AppendChild(new Sheets());
+            
+            var sheet = new Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
+                SheetId = 1,
+                Name = "Image"
+            };
+            sheets.Append(sheet);
+
+            var worksheet = worksheetPart.Worksheet;
+            var sheetData = worksheet.GetFirstChild<SheetData>();
+
+            for (uint row = 1; row <= 10; row++)
+            {
+                var r = new Row() { RowIndex = row };
+                sheetData.Append(r);
+
+                Cell previousCell = null;
+                for (uint col = 1; col <= 10; col++)
+                {
+                    char columnName = Convert.ToChar(col + 64);
+
+                    Cell newCell = new Cell() { CellReference = $"{columnName}{row}" };
+                    r.InsertAfter(newCell, previousCell);
+                    previousCell = newCell;
+
+                    newCell.CellValue = new CellValue(newCell.CellReference);
+                    newCell.DataType = new EnumValue<CellValues>(CellValues.String);
+                }
+            }
+
+            worksheet.Save();
+            workbookpart.Workbook.Save();
+            spreadsheetDocument.Close();
+
+            Console.WriteLine("Done.");
         }
     }
 }
